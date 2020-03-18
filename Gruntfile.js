f7fa1e2d8267d4e73b3e5a721f3157780800f474
aalixue <2319684501@qq.com>
2020-03-18 20:03:48 +0800

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,31 @@
-/* global module: true */
 module.exports = function (grunt) {
     grunt.initConfig({
-        htmlmin: {
-            options: {
-                removeComments: true,
-                collapseWhitespace: true
-            },
-            files: {
-                src: 'dist/index.html',
-                dest: 'dist/index.html'
-            }
+      htmlmin: {
+        options: {
+          collapseWhitespace: true,
+          preserveLineBreaks: false
+        },
+        files: {
+          src: 'index.html',
+          dest: 'dist/index.html'
+        }
         },
         cssmin: {
-            'dist/register.css': 'register.css'
+            "dist/register.css": "register.css"
         },
         uglify: {
-            release: {
-                files: {
-                    'dist/register.js': 'register.js'
+            release:{
+                files:{
+                    "dist/register.js":"register.js"
                 }
             }
-        },
-        copy: {
-            html: {
-                src: './index.html',
-                dest: './dist/index.html'
-            }
-        },
-        useminPrepare: {
-            html: 'index.html',
-            options: {
-                dest: 'dist'
-            }
-        },
-        usemin: {
-            html: ['dist/index.html']
         }
-
     });
-
+  
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-usemin');
-
-    grunt.registerTask('release', ['copy', 'useminPrepare', 'uglify', 'usemin', 'cssmin', 'htmlmin']);
-
+  
+    grunt.registerTask('default', ['uglify','cssmin','htmlmin']);
+ 
 };
