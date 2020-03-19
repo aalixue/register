b1da62dc8e820a7e13cc43b54282c182653068f3
aalixue <2319684501@qq.com>
2020-03-19 08:04:45 +0800

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,55 @@
+/* global module: true */
 module.exports = function (grunt) {
     grunt.initConfig({
-      htmlmin: {
-        options: {
-          collapseWhitespace: true,
-          preserveLineBreaks: false
-        },
-        files: {
-          src: 'index.html',
-          dest: 'dist/index.html'
-        }
+        htmlmin: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            files: {
+                src: 'dist/index.html',
+                dest: 'dist/index.html'
+            }
         },
         cssmin: {
-            "dist/register.css": "register.css"
+            'dist/register.css': 'register.css'
         },
         uglify: {
-            release:{
-                files:{
-                    "dist/register.js":"register.js"
+            release: {
+                files: {
+                    'dist/register.js': 'register.js'
                 }
             }
+        },
+
+        copy: {
+            html: {
+                src: "./index.html",
+                dest: "./dist/index.html"
+            },
+            css: {
+                src: "./register.css",
+                dest: "./dist/register.css"
+            },
+            js: {
+                src: "./register.js",
+                dest: "./dist/register.js"
+            },
+            image: {
+                expand: true,
+                cwd: './',
+                src: 'image/*.*',
+                dest: 'dist/'
+            }
         }
+
     });
-  
+
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-    grunt.registerTask('default', ['uglify','cssmin','htmlmin']);
- 
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('release', ['copy', 'uglify', 'cssmin', 'htmlmin']);
+
 };
